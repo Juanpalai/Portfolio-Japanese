27b1bbf87f1577c81a0bc42f6ab93a761997fcaa
Glitch (glitch-hello-website) <none>
2021-09-11 09:14:15 +0000

    🌲🌑 Checkpoint ./index.html:33839123/795 ./popup.js:33839123/557

--- popup.js
+++ popup.js
@@ -9,6 +9,22 @@ var btnAbrirPopupMG = document.getElementById("btn-abrir-popupMG"),
   popupmg = document.getElementById("popupMG"),
   btnCerrarPopupMG = document.getElementById("btn-cerrar-popupMG");
 
+var btnAbrirPopupTA = document.getElementById("btn-abrir-popupMG"),
+  overlayta = document.getElementById("overlayTA"),
+  popupta = document.getElementById("popupTA"),
+  btnCerrarPopupTA = document.getElementById("btn-cerrar-popupTA");
+
+btnAbrirPopup.addEventListener("click", function() {
+  overlayc4.classList.add("active");
+  popupc4.classList.add("active");
+});
+
+btnCerrarPopup.addEventListener("click", function(e) {
+  e.preventDefault();
+  overlayc4.classList.remove("active");
+  popupc4.classList.remove("active");
+});
+
 btnAbrirPopup.addEventListener("click", function() {
   overlayc4.classList.add("active");
   popupc4.classList.add("active");
