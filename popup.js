0723e451716bb1414eac9a90c5bcb3c0a2231a8e
Glitch (glitch-hello-website) <none>
2021-09-11 08:27:14 +0000

    📙🐬 Checkpoint ./index.html:33839123/37050 ./popup.js:33839123/2187

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
-var btnAbrirPopup = document.getElementById("btn-abrir-popup"),
-  overlay = document.getElementById("overlay"),
-  popup = document.getElementById("popup"),
+var btnAbrirPopup = document.getElementById("btn-abrir-popupMG"),
+  overlay = document.getElementById("overlayMG"),
+  popup = document.getElementById("popupMG"),
+  btnCerrarPopup = document.getElementById("btn-cerrar-popup");
+
+var btnAbrirPopup = document.getElementById("btn-abrir-popupC4"),
+  overlay = document.getElementById("overlayC4"),
+  popup = document.getElementById("popupC4"),
   btnCerrarPopup = document.getElementById("btn-cerrar-popup");
 
 btnAbrirPopup.addEventListener("click", function() {
