760ab2071c0b770c2f0338dc700087e185746290
Glitch (glitch-hello-website) <none>
2021-12-16 06:56:31 +0000

    🐚🏃 Checkpoint ./connect4.html:33839123/37 ./popup.js:33839123/2699 ./style.css:33839123/467

--- popup.js
+++ popup.js
@@ -1,65 +1,21 @@
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
 
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
 
-var btnAbrirPopup = document.getElementById("btn-abrir-popupC4"),
-  overlayc4 = document.getElementById("overlayC4"),
-  popupc4 = document.getElementById("popupC4"),
-  btnCerrarPopup = document.getElementById("btn-cerrar-popupC4");
-
-var btnAbrirPopupMG = document.getElementById("btn-abrir-popupMG"),
-  overlaymg = document.getElementById("overlayMG"),
-  popupmg = document.getElementById("popupMG"),
-  btnCerrarPopupMG = document.getElementById("btn-cerrar-popupMG");
-
-var btnAbrirPopupTA = document.getElementById("btn-abrir-popupTA"),
-  overlayta = document.getElementById("overlayTA"),
-  popupta = document.getElementById("popupTA"),
-  btnCerrarPopupTA = document.getElementById("btn-cerrar-popupTA");
-
-var btnAbrirPopupWP = document.getElementById("btn-abrir-popupWP"),
-  overlaywp = document.getElementById("overlayWP"),
-  popupwp = document.getElementById("popupWP"),
-  btnCerrarPopupWP = document.getElementById("btn-cerrar-popupWP");
-
-btnAbrirPopupTA.addEventListener("click", function() {
-  overlayta.classList.add("active");
-  popupta.classList.add("active");
-});
-
-btnCerrarPopupTA.addEventListener("click", function(e) {
-  e.preventDefault();
-  overlayta.classList.remove("active");
-  popupta.classList.remove("active");
-});
-
-btnAbrirPopup.addEventListener("click", function() {
-  overlayc4.classList.add("active");
-  popupc4.classList.add("active");
-});
-
-btnCerrarPopup.addEventListener("click", function(e) {
-  e.preventDefault();
-  overlayc4.classList.remove("active");
-  popupc4.classList.remove("active");
-});
-
-btnAbrirPopupMG.addEventListener("click", function() {
-  overlaymg.classList.add("active");
-  popupmg.classList.add("active");
-});
-
-btnCerrarPopupMG.addEventListener("click", function(e) {
-  e.preventDefault();
-  overlaymg.classList.remove("active");
-  popupmg.classList.remove("active");
-});
-
-btnAbrirPopupWP.addEventListener("click", function() {
-  overlaywp.classList.add("active");
-  popupwp.classList.add("active");
-});
-
-btnCerrarPopupWP.addEventListener("click", function(e) {
-  e.preventDefault();
-  overlaywp.classList.remove("active");
-  popupwp.classList.remove("active");
-});
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
